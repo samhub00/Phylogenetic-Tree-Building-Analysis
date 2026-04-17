24595184d136cf162017c198314b3e99b4fb4e5c
Sam Hubler <samhubler148@gmail.com>
2026-04-17 12:37:23 -0700

    started maximum parsimony alignment

---
+++ parsimonyBasedAlignment.py
@@ -0,0 +1,20 @@
+from Bio import AlignIO, Phylo
+import Bio.Phylo.TreeConstruction
+
+#SH
+def parsimonyTreeBuilder(alignment,starter_tree, a_format='phylip',  st_format='newick'):
+    """
+    Max Parsimony Tree building algorithm. Requires alignment file and starting tree file. 
+    Alignment file default format is PHYLIP, and starting tree default file is newick.
+    """
+    aln = AlignIO.read(open(alignment), a_format) 
+
+    starter = Phylo.read(starter_tree, st_format) #newick is tree format default, can change
+
+    scorer = Bio.Phylo.TreeConstruction.ParsimonyScorer()
+    searcher = Bio.Phylo.TreeConstruction.NNITreeSearcher(scorer)
+    constructor = Bio.Phylo.TreeConstruction.ParsimonyTreeConstructor(searcher, starter_tree)
+    pars_tree = constructor.build_tree(aln)
+
+    return pars_tree
+
